3bc61735b9ac95fdb59c5ea2c0613207dd57168a
AKIRA-MIYAKE <akira.aktk.miyake@gmail.com>
2017-12-01 11:26:27 +0900

    chore: correct test dir name

--- __tests__/services/sample/hello.test.ts
+++ __tests__/services/sample/hello.test.ts
@@ -1,3 +1,4 @@
+import { Handler } from 'aws-lambda'
 import { invokeHandler, generateMockCallback } from 'lambda-utilities'
 import { generateDummyAPIGatewayEvent } from 'lamprox'
 
@@ -7,28 +8,32 @@ describe('services/sample/handler', () => {
 
   describe('hello', () => {
 
-    it('Should return response.', async () => {
+    it('Should return response.', done => {
       const event = generateDummyAPIGatewayEvent()
-      const callback = generateMockCallback((error, result) => {
+      const callback = generateMockCallback((error, result: any) => {
         callback.once()
         const body = JSON.parse(result.body)
         expect(body.message).toBe('Hello, Lambda!!')
         expect(callback.verify()).toBe(true)
+
+        done()
       })
 
-      await invokeHandler(hello, { event, callback })
+      invokeHandler(hello as Handler, { event, callback })
     })
 
-    it('Should return response when evnet include query string params.', async () => {
+    it('Should return response when evnet include query string params.', done => {
       const event = generateDummyAPIGatewayEvent({ queryStringParameters: { "name": "John Doe" }})
-      const callback = generateMockCallback((error, result) => {
+      const callback = generateMockCallback((error, result: any) => {
         callback.once()
         const body = JSON.parse(result.body)
         expect(body.message).toBe('Hello, John Doe!!')
         expect(callback.verify()).toBe(true)
+
+        done()
       })
 
-      await invokeHandler(hello, { event, callback })
+      invokeHandler(hello as Handler, { event, callback })
     })
 
 
